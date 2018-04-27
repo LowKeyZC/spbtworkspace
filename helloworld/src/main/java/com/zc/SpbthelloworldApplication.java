540cdd78bcb545d121446aa7832aec52e2140c42
zhouchao <zhouchao2@100tal.com>
2018-04-27 17:05:18 +0800

    springboot工作空间
    Signed-off-by: zhouchao <zhouchao2@100tal.com>

---
+++ helloworld/src/main/java/com/zc/SpbthelloworldApplication.java
@@ -0,0 +1,18 @@
+package com.zc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpbthelloworldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpbthelloworldApplication.class, args);
+	}
+	@RequestMapping("hello")
+	public String helloworld(){
+		return "hello world";
+	}
+}
